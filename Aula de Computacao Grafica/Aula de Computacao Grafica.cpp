fadee0e3e18d9f0e73ac4b22c9f869739afbc88f
ERICK COSTA REIMBERG DE LIMA <erick.crlima@senacsp.edu.br>
2024-08-14 21:03:13 -0300

    Commit da aula 1

--- Aula de Computacao Grafica/Aula de Computacao Grafica.cpp
+++ Aula de Computacao Grafica/Aula de Computacao Grafica.cpp
@@ -1,20 +1,64 @@
-// Aula de Computacao Grafica.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
-//
-
+#include <stdio.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
+
+const GLint WIDTH = 800, HEIGHT = 600;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	//GLEW = GERENCIADOR DE PACOTES DO OPENGL
+	//GLFW = GERENCIADOR DE JANELAS
+
+	if (!glfwInit()) {
+		printf("Não funcionou o GLFW");
+		return 1;
+	}
+
+	//definindo as versoes min e max do glfw
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	//defini para so usar o basico do opengl
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	//compatibilidade entre funcoes antigas
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "computacao-grafica", NULL, NULL);
+
+	if (!window) {
+		printf("Janela não foi aberta");
+		glfwTerminate();
+		return 1;
+	}
+
+	int bufferWidth, bufferHeight;
+	glfwGetFramebufferSize(window, &bufferWidth, &bufferHeight);
+
+	//define que eh esta janela que estamos trabalhando
+	glfwMakeContextCurrent(window);
+
+	//usa as funcoes dos drives da sua placa de video
+	glewExperimental = GL_TRUE;
+
+	if (glewInit() != GLEW_OK) {
+		printf("Nao foi iniciado o GLEW");
+		glfwTerminate();
+		glfwDestroyWindow(window);
+		return 1;
+	}
+
+	glViewport(0, 0, bufferWidth, bufferHeight);
+
+	while (!glfwWindowShouldClose(window)) {
+		//cria um evento
+		glfwPollEvents();
+
+		//define a cor do fundo 
+		glClearColor(1.0f, 0.0f, 0.7f, 0.7f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+	}
 }
-
-// Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
-// Depurar programa: F5 ou menu Depurar > Iniciar Depuração
-
-// Dicas para Começar: 
-//   1. Use a janela do Gerenciador de Soluções para adicionar/gerenciar arquivos
-//   2. Use a janela do Team Explorer para conectar-se ao controle do código-fonte
-//   3. Use a janela de Saída para ver mensagens de saída do build e outras mensagens
-//   4. Use a janela Lista de Erros para exibir erros
-//   5. Ir Para o Projeto > Adicionar Novo Item para criar novos arquivos de código, ou Projeto > Adicionar Item Existente para adicionar arquivos de código existentes ao projeto
-//   6. No futuro, para abrir este projeto novamente, vá para Arquivo > Abrir > Projeto e selecione o arquivo. sln
