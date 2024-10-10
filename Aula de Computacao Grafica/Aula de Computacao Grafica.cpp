c087776c856c3557446e4ab658120947e3a3c05b
erick.crlima <erick.crlima@CAS685516F274D>
2024-10-09 21:01:17 -0300

    Adicionando Texturas ao projeto

--- Aula de Computacao Grafica/Aula de Computacao Grafica.cpp
+++ Aula de Computacao Grafica/Aula de Computacao Grafica.cpp
@@ -1,3 +1,5 @@
+#define STB_IMAGE_IMPLEMENTATION
+
 #include <iostream>
 #include <vector>
 #include "GL/glew.h"
@@ -10,11 +12,14 @@
 #include "Shader.h"
 #include "Window.h"
 #include "Camera.h"
+#include "Texture.h"
 
 std::vector<Mesh*> meshList;
 std::vector<Shader*> shaderList;
 Window* window;
 Camera camera;
+Texture brickTexture;
+Texture groundTexture;
 
 //Vertex Shader
 static const char* vertexLocation = "VertexShader.glsl";
@@ -22,18 +27,30 @@ static const char* fragmentLocation = "FragmentShader.glsl";
 
 void CriaTriangulos() {
 	GLfloat vertices[] = {
-		//x , y		
-		-1.0f, -1.0f, 0.0f,         //Vertice 1 (Preto)
-		0.0f, 1.0f, 0.0f,           //Vertice 0 (Verde)
-		1.0f, -1.0f, 0.0f,          //Vertice 2 (Vermelho)
-		0.0f, -1.0f, 1.0f            //Vertice 3 (Azul)
+		//x,	y,		z
+		-1.0f, -1.0f, 1.0f,		//Vértice 0 (Preto)
+		-1.0f, 1.0f, 1.0f,		//Vértice 1 (Verde)
+		1.0f, 1.0f, 1.0f,		//Vértice 2 (Vermelho)
+		1.0f, -1.0f, 1.0f,		//Vértice 3 (Azul)
+		-1.0f, -1.0f, -1.0f,	//Vértice 4 (Preto)
+		-1.0f, 1.0f, -1.0f,		//Vértice 5 (Verde)
+		1.0f, 1.0f, -1.0f,		//Vértice 6 (Vermelho)
+		1.0f, -1.0f, -1.0f		//Vértice 7 (Azul)
 	};
 
 	GLuint indices[] = {
-		0, 1, 2,
+		0, 1, 3, // Base Inferior
 		1, 2, 3,
-		0, 1, 3,
-		0, 2, 3
+		4, 5, 7, // Base Superior
+		5, 6, 7,
+		2, 3, 6, // Direita
+		7, 3, 6,
+		1, 5, 0,
+		4, 5, 0,
+		1, 5, 2,
+		6, 5, 2,
+		0, 3, 4,
+		7, 3, 4
 	};
 
 	Mesh* obj1 = new Mesh();
@@ -60,13 +77,18 @@ int main() {
 	CreateShader();
 
 	//Criando a camera
-	camera = Camera(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f), -90.0f, 0.0f, 0.005f, 0.005f);
+	camera = Camera(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f), -90.0f, 0.0f, 0.0005f, 0.5f);
+
+	brickTexture = Texture((char*)"Texture/brick.png");
+	brickTexture.loadTexture();
+	groundTexture = Texture((char*)"Texture/ground.png");
+	groundTexture.loadTexture();
 
 	//Variaveis para controle da movimentação do triangulo
 	bool direction = true, sizeDirection = true, angleDirection = true; //true=direita e false=esquerda
 	float triOffset = 0.0f, maxOffset = 0.7f, minOffset = -0.7f, incOffset = 0.01f;
 	float size = 0.4f, maxSize = 0.7f, minSize = -0.7f, incSize = 0.01f;
-	float angle = 0.0f, maxAngle = 360.0f, minAngle = -1.0f, incAngle = 0.5f;
+	float angle = 0.0f, maxAngle = 360.0f, minAngle = -1.0f, incAngle = 0.005f;
 
 	while (!window->ShouldClose()) {
 
@@ -102,23 +124,25 @@ int main() {
 		/*
 		* Triangulo 1
 		*/
+		brickTexture.useTexture();
 		meshList[0]->RenderMesh();
 		//criar uma matriz 4x4 (1.0f)
 		glm::mat4 model(1.0f);
 		model = glm::translate(model, glm::vec3(0.0f, -0.25f, -1.5f)); //Movimentações do triangulo
 		model = glm::scale(model, glm::vec3(0.4, 0.4, 0.4)); //Tamanho do triangulo
-		model = glm::rotate(model, glm::radians(angle), glm::vec3(0.0f, 1.0f, 0.0f)); //Rotação
+		model = glm::rotate(model, glm::radians(angle), glm::vec3(0.0f, 0.5f, 0.0f)); //Rotação
 
 		glUniformMatrix4fv(shader->GetUniformModel(), 1, GL_FALSE, glm::value_ptr(model)); //Envia os dados para o triangulo
 
 		/*
 		* Triangulo 2
 		*/
+		groundTexture.useTexture();
 		meshList[1]->RenderMesh();
 		model = glm::mat4(1.0f);
 		model = glm::translate(model, glm::vec3(0.0f, 0.5f, -2.0f)); //Movimentações do triangulo
 		model = glm::scale(model, glm::vec3(0.4, 0.4, 0.4)); //Tamanho do triangulo
-		model = glm::rotate(model, glm::radians(angle), glm::vec3(0.0f, -1.0f, 0.0f)); //Rotação
+		model = glm::rotate(model, glm::radians(angle), glm::vec3(0.0f, -0.5f, 0.0f)); //Rotação
 		glUniformMatrix4fv(shader->GetUniformModel(), 1, GL_FALSE, glm::value_ptr(model)); //Envia os dados para o triangulo
 
 
--- Aula de Computacao Grafica/Mesh.cpp
+++ Aula de Computacao Grafica/Mesh.cpp
@@ -6,50 +6,40 @@ Mesh::Mesh() {
 	IBO = 0;
 }
 
+void Mesh::CreateMesh(GLfloat* vertices, unsigned int numOfVertices, GLuint* indices, unsigned int numOfIndices) {
+	Mesh::numIndices = numOfIndices;
+
+	glGenVertexArrays(1, &VAO); //Cria o VAO
+	glBindVertexArray(VAO); //Coloca o VAO em contexto
+
+	glGenBuffers(1, &IBO); //Cria o IBO
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO); //Coloca o IBO em contexto
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, numOfIndices, indices, GL_STATIC_DRAW); //Explica o valor do Array
+
+	glGenBuffers(1, &VBO); //Cria o VBO
+	glBindBuffer(GL_ARRAY_BUFFER, VBO); //Coloca o VBO em contexto
+	glBufferData(GL_ARRAY_BUFFER, numOfVertices, vertices, GL_STATIC_DRAW); //Explica o valor do Array
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0); //Explica os valores de x, y e z
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, 0); //Explica os valores da textura
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0); //remover do contexto o IBO
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0); //remover do contexto o VBO
+	glBindVertexArray(0); //remover do contexto o VAO
+}
+
+void Mesh::RenderMesh() {
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO); //Coloca o IBO em contexto
+	glDrawElements(GL_TRIANGLES, numIndices, GL_UNSIGNED_INT, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0); //remover do contexto o IBO
+	glBindVertexArray(0);
+}
+
 Mesh::~Mesh() {
 	if (VBO != 0) glDeleteBuffers(1, &VBO);
 	if (IBO != 0) glDeleteBuffers(1, &IBO);
 	if (VAO != 0) glDeleteBuffers(1, &VAO);
-}
-
-void Mesh::CreateMesh(GLfloat* vertices, unsigned int numVertices, GLuint* indices, unsigned int numIndices) {
-	Mesh::numIndices = numIndices;
-
-	glGenVertexArrays(1, &VAO); //Gera um VAO ID
-	glBindVertexArray(VAO); //Atribuindo o ID ao VAO
-
-	glGenBuffers(1, &IBO);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, numIndices, indices, GL_STATIC_DRAW);
-
-	//Carregar os dados de vértice para a placa de vídeo
-	//Vertex Buffer Object: VBO
-	glGenBuffers(1, &VBO); //Gera um VBO ID
-	glBindBuffer(GL_ARRAY_BUFFER, VBO); //Transforma o VBO em um Array Buffer
-	glBufferData(GL_ARRAY_BUFFER, numVertices, vertices, GL_STATIC_DRAW); //Copia os dados ao VBO
-
-	//GL_STATIC_DRAW: Os dados do vértice serão carregados uma vez e desenhados várias vezes (por exemplo, o mundo).
-	//GL_DYNAMIC_DRAW: Os dados do vértice serão criados uma vez, alterados de tempos em tempos, mas desenhados muitas vezes mais do que isso.
-	//GL_STREAM_DRAW : Os dados do vértice serão carregados uma vez e desenhados uma vez.
-
-	//Vertex Attribute Pointer - Atributos dos dados na memória
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0); //0: shader location | 2: numero de valores de vertice (x,y) | GL_FLOAT: tipo dos valores
-	//GL_FALSE: normalizado | 0: pular elemento (cor) | 0: offset
-	//Vertex Attribute Pointer Enable
-	glEnableVertexAttribArray(0); //0: shader location
-
-	//Limpar o Buffer
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	//Limpar o Vertex Array
-	glBindVertexArray(0);
-}
-
-//TODO: Fazer a renderizacao do cubo
-void Mesh::RenderMesh() {
-	glBindVertexArray(VAO); //Bind o VAO
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
-	glDrawElements(GL_TRIANGLES, Mesh::numIndices, GL_UNSIGNED_INT, 0);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 }
